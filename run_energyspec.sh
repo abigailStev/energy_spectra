3dd11a33799a3029ca0d71136948edef17e4dea4
Abigail Stevens <A.L.Stevens@uva.nl>
2014-07-16 16:43:15 +0200

    Added mean_spectrum.py to it, commented out old ratio spectrum stuff.

--- run_energyspec.sh
+++ run_energyspec.sh
@@ -8,14 +8,14 @@
 
 home_dir=$(ls -d ~)  # the -d flag is extremely important here
 in_dir="$home_dir/Dropbox/Research/cross_correlation"
-exec_dir="$home_dir/Dropbox/Research/energy_spectra"
-out_dir="$exec_dir/out_es"
+exe_dir="$home_dir/Dropbox/Research/energy_spectra"
+out_dir="$exe_dir/out_es"
 script_dir="$home_dir/Dropbox/Scripts"
-# day=$(date +%y%m%d)  # make the date a string and assign it to 'day', for filename
-day="140624"
+day=$(date +%y%m%d)  # make the date a string and assign it to 'day'
+day="140715"
 
-if test ! -d "$out_dir"
-   	then mkdir -p "$out_dir"
+if [ ! -d "$out_dir" ]; then
+	mkdir -p "$out_dir"
 fi
 
 propID=$1
@@ -35,6 +35,7 @@ quaternions="$data_dir/appx_quat.fits"
 tab_ext="dat"
 plot_ext="png"
 
+## Making response matrix, getting exposure time of observation
 if [ -e "$rsp_matrix" ]; then
 	echo "$rsp_matrix already exists."
 elif [ -e "$data_dir/event.pha" ] && [ -e "${quaternions}" ]; then
@@ -44,28 +45,47 @@ else
 fi
 if [ -e "$in_file" ]; then
 	obs_time=$(python -c "from tools import read_obs_time; print read_obs_time('$in_file')")
-	echo "OBS TIME = " $obs_time "seconds"
+	echo "OBS TIME =" $obs_time "seconds"
 else
 	obs_time = 0
 	echo -e "\n\t Couldn't get observation time from header, set obs_time to zero.\n"
 fi
 
-for pbin in {25..26}; do
+## Making file with list of event.pha spectra
+spectra_list="$out_dir/${propID}_${day}_t${dt}_${numsec}sec_spectra.lst"
+if [ -e "$spectra_list" ]; then
+	rm "$spectra_list"
+fi
+touch "$spectra_list"
+echo "$spectra_list"
+
+for obsID in $(cat $obsID_list); do
+	spec_file="$home_dir/Reduced_data/$propID/$obsID/event.pha"
+# 	echo "$spec_file"
+	if [ -e "$spec_file" ]; then
+ 		echo "$spec_file" >> $spectra_list
+ 	fi
+done
+python "$exe_dir"/mean_spectrum.py -i "$spectra_list" -o out.dat -c "$in_file"
+
+
+## Generating energy spectra at each phase bin
+for pbin in {25..25}; do
 
 	out_end="${propID}_${day}_t${dt}_${numsec}sec_pbin_${pbin}"
 	out_file="$out_dir/$out_end"
 
-	if [ -e "$in_file" ]; then
-		python "$exec_dir"/energyspec.py "$in_file" "${out_file}.${tab_ext}" "$pbin" "$filt_loc"
-	else
-		echo -e "\n\t $in_file does not exist, energyspec.py was NOT run.\n"
-	fi
-	
-	pwd
-	if [ -e "$rsp_matrix" ] && [ -e "${out_file}.${tab_ext}" ]; then
-		
-		cd "$out_dir"
-		# ascii2pha infile="${out_end}.${tab_ext}" \
+# 	if [ -e "$in_file" ]; then
+# 		python "$exe_dir"/energyspec.py -i "$in_file" -o "${out_file}.${tab_ext}" -b "$pbin" -f "$filt_loc"
+# 	else
+# 		echo -e "\n\t $in_file does not exist, energyspec.py was NOT run.\n"
+# 	fi
+# 	
+# 	pwd
+# 	if [ -e "$rsp_matrix" ] && [ -e "${out_file}.${tab_ext}" ]; then
+# 		
+# 		cd "$out_dir"
+# 			ascii2pha infile="${out_end}.${tab_ext}" \
 # 			outfile="${out_end}.pha" \
 # 			chanpres=no \
 # 			dtype=2 \
@@ -83,38 +103,38 @@ for pbin in {25..26}; do
 # 			respfile="$rsp_matrix"
 # 		echo "XSPEC data: ${out_file}.pha"
 # 		echo -e "XSPEC resp: $rsp_matrix\n"
-	else
-		echo -e "\n\t ${rsp_matrix} and/or ${out_file}.${tab_ext} do NOT exist, ascii2pha was NOT run.\n"
-	fi
+# 	else
+# 		echo -e "\n\t${rsp_matrix} and/or ${out_file}.${tab_ext} do NOT exist, ascii2pha was NOT run.\n"
+# 	fi
 	
-	if [ -e "${out_file}.${tab_ext}" ]; then
-		python "$exec_dir"/ratio_spectrum.py "${out_file}.${tab_ext}" "$obsID_list" "${out_file}_rwm.${tab_ext}"
-	else
-		echo -e "\n\t ${out_file}.${tab_ext} does not exist, ratio_spectrum.py was NOT run."
-	fi
-	if [ -e "$rsp_matrix" ] && [ -e "${out_file}_rwm.${tab_ext}" ]; then
-		cd "$out_dir"
-		ascii2pha infile="${out_end}_rwm.${tab_ext}" \
-			outfile="${out_end}_rwm.pha" \
-			chanpres=no \
-			dtype=2 \
-			qerror=yes \
-			rows=- \
-			fchan=0 \
-			tlmin=0 \
-			detchans=64 \
-			telescope=RXTE \
-			instrume=PCA \
-			detnam=PCU2 \
-			filter=NONE \
-			exposure=$obs_time \
-			clobber=yes \
-			respfile="$rsp_matrix"
-		echo "XSPEC data: ${out_file}_rwm.pha"
-		echo -e "XSPEC resp: $rsp_matrix\n"
-	else
-		echo -e "\n\t ${rsp_matrix} and/or ${out_file}_rwm.${tab_ext} do NOT exist, ascii2pha was NOT run."
-	fi
+# 	if [ -e "${out_file}.${tab_ext}" ]; then
+# 		python "$exe_dir"/ratio_spectrum.py "${out_file}.${tab_ext}" "$obsID_list" "${out_file}_rwm.${tab_ext}"
+# 	else
+# 		echo -e "\n\t ${out_file}.${tab_ext} does not exist, ratio_spectrum.py was NOT run."
+# 	fi
+# 	if [ -e "$rsp_matrix" ] && [ -e "${out_file}_rwm.${tab_ext}" ]; then
+# 		cd "$out_dir"
+# 		ascii2pha infile="${out_end}_rwm.${tab_ext}" \
+# 			outfile="${out_end}_rwm.pha" \
+# 			chanpres=no \
+# 			dtype=2 \
+# 			qerror=yes \
+# 			rows=- \
+# 			fchan=0 \
+# 			tlmin=0 \
+# 			detchans=64 \
+# 			telescope=RXTE \
+# 			instrume=PCA \
+# 			detnam=PCU2 \
+# 			filter=NONE \
+# 			exposure=$obs_time \
+# 			clobber=yes \
+# 			respfile="$rsp_matrix"
+# 		echo "XSPEC data: ${out_file}_rwm.pha"
+# 		echo -e "XSPEC resp: $rsp_matrix\n"
+# 	else
+# 		echo -e "\n\t ${rsp_matrix} and/or ${out_file}_rwm.${tab_ext} do NOT exist, ascii2pha was NOT run."
+# 	fi
 
 done
 

42c877612658f6a49a6e89a5c105c7e281fb78b9
Abigail Stevens <A.L.Stevens@uva.nl>
2015-05-26 17:12:29 +0200

    Beginning to allow for any other 3rd parameter, not just disk bb Tin. Unsure if I want to keep going with this. Many print statemets for debugging why theres an error for disk bb norm.

--- multifit_plots.py
+++ multifit_plots.py
@@ -135,7 +135,7 @@ def read_log_file(log_file):
     assert len(bb_t) == len(bb_t_err), "ERROR: Issue with reading log file. "\
             "Length of Tin != length of error on Tin."
     # assert len(bb_norm) == len(bb_norm_err), "ERROR: Issue with reading log "\
-    #       "file. Length of diskbb norm != length of error on diskbb norm."
+    #         "file. Length of diskbb norm != length of error on diskbb norm."
     assert len(line_e) == len(line_e_err), "ERROR: Issue with reading log "\
             "file. Length of LineE != length of error on LineE."
     assert len(sigma) == len(sigma_err), "ERROR: Issue with reading log file. "\
@@ -165,7 +165,8 @@ def read_log_file(log_file):
 
 ################################################################################
 def make_plots_var3(plot_name, num_spectra, fracsctr, fracsctr_err, gamma, \
-                    gamma_err, bb_t, bb_t_err, fs_sinefit, bbt_sinefit):
+                    gamma_err, param, param_err, fs_sinefit, param_sinefit,
+                    param_title):
     """
     Making plots of fit parameters vs phase for three co-varying parameters.
 
@@ -177,7 +178,7 @@ def make_plots_var3(plot_name, num_spectra, fracsctr, fracsctr_err, gamma, \
     phase = np.arange(num_spectra) / 23.646776
     tinybins = np.arange(-0.01, 1.03, 0.01)
     fs_max = tinybins[np.argmax(fs_sinefit)]
-    bbt_max = tinybins[np.argmax(bbt_sinefit)]
+    param_max = tinybins[np.argmax(param_sinefit)]
 
 
     print "Plot file: %s" % plot_name
@@ -217,18 +218,18 @@ def make_plots_var3(plot_name, num_spectra, fracsctr, fracsctr_err, gamma, \
 
     ax2 = fig.add_subplot(312, sharex=ax1)
 
-    ax2.plot(tinybins, bbt_sinefit, c='black', lw=2)
-    ax2.errorbar(phase, bb_t, yerr=bb_t_err, lw=0, ecolor='green', \
+    ax2.plot(tinybins, param_sinefit, c='black', lw=2)
+    ax2.errorbar(phase, param, yerr=param_err, lw=0, ecolor='green', \
                  marker='.', ms=10, mec='green', mfc='green', elinewidth=2,
                  capsize=2)
-    ax2.vlines(bbt_max, 0.798, 0.810, lw=1, linestyles='dashed')
-    # ax2.vlines(bbt_max - 4.3787e-03, 0.798, 0.810, lw=1, linestyles='dotted')
-    # ax2.vlines(bbt_max + 4.3787e-03, 0.798, 0.810, lw=1, linestyles='dotted')
+    ax2.vlines(param_max, 0.798, 0.810, lw=1, linestyles='dashed')
+    # ax2.vlines(param_max - 4.3787e-03, 0.798, 0.810, lw=1, linestyles='dotted')
+    # ax2.vlines(param_max + 4.3787e-03, 0.798, 0.810, lw=1, linestyles='dotted')
     ax2.tick_params(axis='x', labelsize=18, bottom=True, top=True, \
                     labelbottom=False, labeltop=False)
     ax2.tick_params(axis='y', labelsize=18, left=True, right=True, \
                     labelleft=True, labelright=False)
-    ax2.set_ylabel(r'diskbb: T$_{in}$ (keV)', fontproperties=font_prop)
+    ax2.set_ylabel(param_title, fontproperties=font_prop)
 
     ax2.set_xlim(-0.01, 1.02)
     y_maj_loc = ax2.get_yticks()
@@ -276,10 +277,10 @@ def make_plots_var3(plot_name, num_spectra, fracsctr, fracsctr_err, gamma, \
 def make_plots_var2(plot_name, num_spectra, fracsctr, fracsctr_err, param, \
                     param_err, param_title):
     """
-	Making plots of fit parameters vs phase for two co-varying parameters,
-	fracsctr and another.
-	
-	"""
+    Making plots of fit parameters vs phase for two co-varying parameters,
+    fracsctr and another.
+
+    """
 
     font_prop = font_manager.FontProperties(size=20)
     xLocator = MultipleLocator(0.05)  ## loc of minor ticks on y-axis
@@ -427,7 +428,9 @@ def get_phase(sed_parameters, sed_err, num_spectra):
 
     p_best = leastsq(sine_residuals, p, args=(sed_parameters, sed_err, t), \
             full_output=1)
+    print "P best:", p_best
     best_fit = p_best[0]
+    print "Best fit:", best_fit
 
     plt.errorbar(t, sed_parameters, xerr=None, yerr=sed_err)
     plt.plot(t, sinewave(t, best_fit))
@@ -442,6 +445,8 @@ def get_phase(sed_parameters, sed_err, num_spectra):
     ## As outlined in the scipy.optimize.leastsq documentation, multiply the
     ## bonus matrix by the variance of the residuals to get the covariance
     ## matrix.
+    print "Bonus matrix:", bonus_matrix
+    print "Resid var:", resid_var
     cov_matrix = bonus_matrix * resid_var
 
     tiny_bins = np.arange(-0.01, 1.03, 0.01)
@@ -468,13 +473,12 @@ def main(log_file):
     ##########################################
 
     gamma, gamma_err, fracsctr, fracsctr_err, bb_t, bb_t_err, bb_norm, \
-    bb_norm_err, line_e, line_e_err, sigma, sigma_err, e_norm, e_norm_err, \
-    num_spectra = read_log_file(log_file)
+            bb_norm_err, line_e, line_e_err, sigma, sigma_err, e_norm, \
+            e_norm_err, num_spectra = read_log_file(log_file)
 
     print "Number of spectra:", num_spectra
 
-    print "norm(BB) = ", bb_norm[1]
-
+    # print "norm(BB) = ", bb_norm[1]
     # print np.mean(bb_norm)
     # print np.min(bb_norm)
     # print np.max(bb_norm)
@@ -491,50 +495,68 @@ def main(log_file):
     var_sigma = True
     var_e_norm = True
 
-    if gamma[0] == gamma[1] and gamma_err[1].is_integer():
+    if gamma[0] == gamma[1] and gamma_err[0].is_integer():
         gamma_err = np.repeat(gamma_err[0], num_spectra)
         var_gamma = False
 
-    if fracsctr[0] == fracsctr[1] and fracsctr_err[1].is_integer():
+    if fracsctr[0] == fracsctr[1] and fracsctr_err[0].is_integer():
         fracsctr_err = np.repeat(fracsctr_err[0], num_spectra)
         var_fracsctr = False
 
-    if bb_t[0] == bb_t[1] and bb_t[1].is_integer():
+    if (len(bb_t) == 1 or bb_t[0] == bb_t[1]) and bb_t[0].is_integer():
         bb_t_err = np.repeat(bb_t_err[0], num_spectra)
         var_bb_t = False
 
-    # if bb_norm[0] == bb_norm[1] and bb_norm_err[1].is_integer():
-    #     bb_norm_err = np.repeat(bb_norm_err[0], num_spectra)
-    #     var_bb_norm = False
+    if (bb_norm[0] == bb_norm[1] or len(bb_norm) == 1) and \
+            (np.size(bb_norm_err) == 0 or bb_norm_err[0].is_integer()):
+        if np.size(bb_norm_err) == 0:
+            bb_norm_err = [0.0]
+        bb_norm_err = np.repeat(bb_norm_err[0], num_spectra)
+        var_bb_norm = False
 
-    if line_e[0] == line_e[1] and line_e_err[1].is_integer():
+    if line_e[0] == line_e[1] and line_e_err[0].is_integer():
         line_e_err = np.repeat(line_e_err[0], num_spectra)
         var_line_e = False
 
-    if sigma[0] == sigma[1] and sigma_err[1].is_integer():
+    if sigma[0] == sigma[1] and sigma_err[0].is_integer():
         sigma_err = np.repeat(sigma_err[0], num_spectra)
         var_sigma = False
 
-    if e_norm[0] == e_norm[1] and e_norm_err[1].is_integer():
+    if e_norm[0] == e_norm[1] and e_norm_err[0].is_integer():
         e_norm_err = np.repeat(e_norm_err[0], num_spectra)
         var_e_norm = False
 
-    bb_t_phase, bb_t_phase_err, bb_t_sinefit = get_phase(bb_t, bb_t_err, \
-        num_spectra)
+    ######################################################################
+    ## Computing the phase of the best-fit sine wave and phase difference
+    ######################################################################
+
     fracsctr_phase, fracsctr_phase_err, fracsctr_sinefit = get_phase(fracsctr,\
-        fracsctr_err, num_spectra)
+            fracsctr_err, num_spectra)
 
-    print "diskBB Tin phase: %.4e +- %.4e" % (bb_t_phase, bb_t_phase_err)
+    print "simpl fracsctr phase: %.4e +- %.4e" % (fracsctr_phase, \
+            fracsctr_phase_err)
 
-    print "simpl fracsctr phase: %.4e +- %.4e" % (fracsctr_phase, fracsctr_phase_err)
+    if var_bb_t:
+        bb_t_phase, bb_t_phase_err, bb_t_sinefit = get_phase(bb_t, bb_t_err, \
+                num_spectra)
+        phase_2 = bb_t_phase
+        print "diskBB Tin phase: %.4e +- %.4e" % (bb_t_phase, bb_t_phase_err)
 
-    phase_diff = fracsctr_phase - bb_t_phase
+    if var_bb_norm:
+        print "\tIN HERE"
+        bb_norm_phase, bb_norm_phase_err, bb_norm_sinefit = get_phase(bb_norm, \
+                bb_norm_err, num_spectra)
+        phase_2 = bb_norm_phase
+        print "diskBB norm. phase: %.4e +- %.4e" % (bb_norm_phase, \
+                bb_norm_phase_err)
+
+    phase_diff = fracsctr_phase - phase_2
     if phase_diff < 0:
         phase_diff += 1.0
     elif phase_diff > 1.0:
         phase_diff -= 1.0
 
-    print "Phase diff: %.4f\%" % phase_diff
+    print "Phase diff: %.4f" % phase_diff
 
     ###################
     ## Make the plots!
@@ -544,7 +566,12 @@ def main(log_file):
 
     if var_fracsctr and var_gamma and var_bb_t:
         make_plots_var3(plot_name, num_spectra, fracsctr, fracsctr_err, gamma, \
-                gamma_err, bb_t, bb_t_err, fracsctr_sinefit, bb_t_sinefit)
+                gamma_err, bb_t, bb_t_err, fracsctr_sinefit, bb_t_sinefit, \
+                r"diskBB: T$_{in}$ (kev)")
+    elif var_fracsctr and var_gamma and var_bb_norm:
+        make_plots_var3(plot_name, num_spectra, fracsctr, fracsctr_err, gamma, \
+                gamma_err, bb_norm, bb_norm_err, fracsctr_sinefit, \
+                bb_norm_sinefit, "diskBB: norm.")
     elif var_fracsctr and var_gamma:
         make_plots_var2(plot_name, num_spectra, fracsctr, fracsctr_err, gamma, \
                 gamma_err, r"simpl: $\Gamma$")
